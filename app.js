75802c0e893ce810a42ed14379d4ad87f322d2b2
Jason Barrows <jasonbarrows@gmail.com>
2023-06-26 16:56:02 +0100

    Add API description endpoint

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { getAllTopics } = require('./controllers/topics.controller');
 const { handleServerErrors } = require('./errors');
+const { getAllEndpoints } = require('./controllers/api.controllers');
 
 const app = express();
 
+app.get('/api', getAllEndpoints);
 app.get('/api/topics', getAllTopics);
 
 app.use(handleServerErrors);
