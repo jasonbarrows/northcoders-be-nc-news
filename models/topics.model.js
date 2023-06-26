36dbd2f6e2b48d88805a320c59ab24554f3859e8
Jason Barrows <jasonbarrows@gmail.com>
2023-06-26 14:59:00 +0100

    Update query for selecting topics

--- models/topics.model.js
+++ models/topics.model.js
@@ -2,7 +2,7 @@ const db = require('../db/connection');
 
 exports.selectAllTopics = () => {
   return db.query(
-    `SELECT slug, description FROM topics;`
+    `SELECT * FROM topics;`
   ).then(({ rows }) => {
     return rows;
   });
