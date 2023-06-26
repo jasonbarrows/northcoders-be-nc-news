2d41b8c2a0c4331d4c947ab0195bbf97a16cd86a
Jason Barrows <jasonbarrows@gmail.com>
2023-06-26 14:55:31 +0100

    Add catch block to getAllTopics controller method

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,7 @@
 const { selectAllTopics } = require("../models/topics.model");
 
-exports.getAllTopics = (req, res) => {
+exports.getAllTopics = (req, res, next) => {
   selectAllTopics().then((topics) => {
     res.status(200).send({ topics });
-  });
+  }).catch(next);
 };
