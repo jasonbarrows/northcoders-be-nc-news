9f044ff8797f3c4d622e52b35b0652aebe5d8f51
Jason Barrows <jasonbarrows@gmail.com>
2023-06-29 17:33:44 +0100

    Add endpoint to get user by username

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllUsers } = require('../controllers/users.controllers');
+const { getAllUsers, getUserByUsername } = require('../controllers/users.controllers');
 
 const usersRouter = express.Router();
 
 usersRouter.get('/', getAllUsers);
+usersRouter.get('/:username', getUserByUsername);
 
 module.exports = usersRouter;
